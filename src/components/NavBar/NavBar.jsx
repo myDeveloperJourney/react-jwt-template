789e74a9449defe0520ec17e6a7ee3556a08d679
Daniel Scott <daniel@mydeveloperjourney.com>
2025-04-03 13:28:16 -0500

    wip: conditionally render nav links based on user state

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react';
 import { Link } from 'react-router';
+import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
+    const { user } = useContext(UserContext);
     return (
         <nav>
             <ul>
-                <li>
-                    <Link to="/sign-up">
-                        Sign Up
-                    </Link>
-                </li>
+                {user ? (
+                    <li>Welcome, {user.username}</li>
+                ) : (
+                    <li>
+                        <Link to="/sign-up">
+                            Sign Up
+                        </Link>
+                    </li>
+                )}
             </ul>
         </nav>
     );
