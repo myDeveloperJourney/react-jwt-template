ebc1de28da50886c47dc8796bc754eb1e55c087b
Daniel Scott <daniel@mydeveloperjourney.com>
2025-04-03 13:39:35 -0500

    wip: intialized user state from localstorage

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -2,8 +2,15 @@ import { createContext, useState } from 'react';
 
 const UserContext = createContext();
 
+const getUserFromToken = () => {
+    const token = localStorage.getItem('token');
+    if(!token) return null; 
+    // user is probably logged out or they never signed up
+    return JSON.parse(atob(token.split('.')[1])).payload;
+}
+
 function UserProvider({ children }) {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(getUserFromToken());
     // the children prop represents each component we provide context to
     
     const value = { user, setUser };
