2d830693b1a71e4650e44977e79f008370c8cdef
Daniel Scott <daniel@mydeveloperjourney.com>
2025-04-03 16:42:24 -0500

    wip: adds completed code from challenge

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,17 +1,30 @@
-import { useContext } from 'react';
+import { useEffect, useContext } from 'react';
 import { UserContext } from '../../contexts/UserContext';
+import { index } from '../../services/userService';
 
 const Dashboard = () => {
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-    return (
-        <main>
-            <h1>Welcome, {user.username}</h1>
-            <p>
-                This is the dashboard page where you can see a list of all the users
-            </p>
-        </main>
-    );
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const fetchedUsers = await index();
+        console.log(fetchedUsers);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    if (user) fetchUsers();
+  }, [user]);
+
+  return (
+    <main>
+      <h1>Welcome, {user.username}</h1>
+      <p>
+        This is the dashboard page where you can see a list of all the users.
+      </p>
+    </main>
+  );
 };
 
 export default Dashboard;
